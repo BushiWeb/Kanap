735e7a20bc77923fa725add0eae588fe1ad26859
Emeric <emeric.francisod@gmail.com>
2022-03-04 22:24:21 +0100

    Update ConfigManager documentation and move config
    Documentation wasn't up to date with last changes
    
    Configuration file is now in the production environment to allow for
    easy modifications

--- front/src/js/config/config-manager.js
+++ front/src/js/config/config-manager.js
@@ -1,19 +1,17 @@
-import { CONFIG } from "./config";
-
 /**
  * Class managing configuration data.
  */
 export class ConfigManager {
     /**
      * Get data from the given JSON Object and store those data in the data property.
-     * @param {string} data - Configuration object. The default value is the default configuration object.
+     * @param {mixed} data - Configuration object. Can also be a stringifyed version of the object. The default value is the default configuration object.
      */
     constructor(data = CONFIG) {
         this.data = (typeof data === "string")? JSON.parse(data) : data;
     }
 
     /**
-     * Get API URL from config file.
+     * Build API URL from config data.
      * @returns {string} API URL
      */
     getApiUrl() {
