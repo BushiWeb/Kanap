7f1c8aa0415c2f77a92f8939b07d667926379b67
Emeric Francisod <emeric.francisod+github@gmail.com>
2022-03-23 08:25:00 +0100

    Create Product entity

---
+++ front/src/js/entity/Product.js
@@ -0,0 +1,84 @@
+/**
+ * Class representing a product from the API
+ */
+export class Product {
+    /**
+     * Construct the product
+     * @param {string} id - Product's ID
+     * @param {string} name - Product's name
+     * @param {number} price - Product's price
+     * @param {string} description - Product's description
+     * @param {string} imageSource - Associated image's URL
+     * @param {string} imageAltText - Alternative text of the associated image
+     * @param {string[]} colors - Array of available colors
+     */
+    constructor(id, name, price, description, imageSource, imageAltText, colors) {
+        this._id = id;
+        this._name = name;
+        this._price = price;
+        this._description = description;
+        this._imageSource = imageSource;
+        this._imageAltText = imageAltText;
+        this._colors = [...colors];
+    }
+
+
+    /********************************************
+     * GETTERS
+     ********************************************/
+    /**
+     * _id property getter
+     * @return {string} Return the value of the _id property
+     */
+    get id() {
+        return this._id;
+    }
+
+    /**
+     * _name property getter
+     * @return {string} Return the value of the _name property
+     */
+    get name() {
+        return this._name;
+    }
+
+    /**
+     * _description property getter
+     * @return {string} Return the value of the _description property
+     */
+    get description() {
+        return this._description;
+    }
+
+    /**
+     * _price property getter
+     * @return {string} Return the value of the _price property
+     */
+    get price() {
+        return this._price;
+    }
+
+    /**
+     * _imageSource property getter
+     * @return {string} Return the value of the _imageSource property
+     */
+    get imageSource() {
+        return this._imageSource;
+    }
+
+    /**
+     * _imageAltText property getter
+     * @return {string} Return the value of the _imageAltText property
+     */
+    get imageAltText() {
+        return this._imageAltText;
+    }
+
+    /**
+     * _colors property getter
+     * @return {string} Return the value of the _colors property
+     */
+    get colors() {
+        return this._colors;
+    }
+}
---
+++ front/src/js/entity/Product.unit.test.js
@@ -0,0 +1,54 @@
+import { Product } from "./Product";
+
+import { MOCKED_API_DATA } from '../api/mockedApiData';
+
+describe('Product Unit Test Suite', () => {
+
+    describe('constructor() Test Suite', () => {
+        it('should create an instance of Product with the right informations', () => {
+            const testProduct = MOCKED_API_DATA[0];
+            const productEntity = new Product(testProduct._id, testProduct.name, testProduct.price, testProduct.description, testProduct.imageUrl, testProduct.altTxt, testProduct.colors);
+            expect(productEntity._id).toBe(testProduct._id);
+            expect(productEntity._name).toBe(testProduct.name);
+            expect(productEntity._price).toBe(testProduct.price);
+            expect(productEntity._description).toBe(testProduct.description);
+            expect(productEntity._imageSource).toBe(testProduct.imageUrl);
+            expect(productEntity._imageAltText).toBe(testProduct.altTxt);
+            expect(productEntity._colors).toEqual(testProduct.colors);
+        });
+    });
+
+
+    describe('Getters Test Suite', () => {
+        const testProduct = MOCKED_API_DATA[0];
+        const productEntity = new Product(testProduct._id, testProduct.name, testProduct.price, testProduct.description, testProduct.imageUrl, testProduct.altTxt, testProduct.colors);
+
+        it('should return the value of Product._id', () => {
+            expect(productEntity.id).toBe(productEntity._id);
+        });
+
+        it('should return the value of Product._name', () => {
+            expect(productEntity.name).toBe(productEntity._name);
+        });
+
+        it('should return the value of Product._price', () => {
+            expect(productEntity.price).toBe(productEntity._price);
+        });
+
+        it('should return the value of Product._description', () => {
+            expect(productEntity.description).toBe(productEntity._description);
+        });
+
+        it('should return the value of Product._imageSource', () => {
+            expect(productEntity.imageSource).toBe(productEntity._imageSource);
+        });
+
+        it('should return the value of Product._imageAltText', () => {
+            expect(productEntity.imageAltText).toBe(productEntity._imageAltText);
+        });
+
+        it('should return the value of Product._colors', () => {
+            expect(productEntity.colors).toBe(productEntity._colors);
+        });
+    });
+});
